84aa0d7a7435ee6a45730590217b3d7891b8a03f
Adriana Riofrio <alriofri@espol.edu.ec>
2020-06-24 10:30:01 -0500

    Ejemplo ISP

---
+++ src/tema1/ISP.java
@@ -0,0 +1,92 @@
+package tema1;
+
+
+
+public class ISP {
+	
+	
+	interface Mamifero {
+		public void amamantar();
+		
+		
+	}
+	
+	interface MCorredor {
+		public void correr();
+
+	}
+	
+	interface MPonedorHuevos {
+		public void ponerHuevos();
+
+	}
+	
+	interface MNadador {
+		
+		public void nadar();
+
+	
+	
+	class Ornitorrinco implements Mamifero, MPonedorHuevos, MNadador {
+
+		@Override
+		public void nadar() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void amamantar() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void ponerHuevos() {
+			// TODO Auto-generated method stub
+			
+		}
+
+	}
+
+	
+	class Perro implements Mamifero, MCorredor{
+
+		@Override
+		public void correr() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void amamantar() {
+			// TODO Auto-generated method stub
+			
+		}
+
+	}
+	
+	class Delfin implements Mamifero, MNadador {
+
+
+		@Override
+		public void amamantar() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void nadar() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		
+	}
+
+	}
+	
+	
+
+
+}
