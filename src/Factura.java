eb7c44570e5558146c87ee22cb1a3c293cb79011
Ricardo Rivera <rarivera@espol.edu.ec>
2020-06-24 10:26:00 -0500

    Single responsability principle

---
+++ src/Factura.java
@@ -0,0 +1,10 @@
+
+public class Factura {
+
+	public void calcularImpuestosFactura() {
+		//calcula los impuestos asociados a la compra
+	}
+	public void generarFactura() {
+		//generar una nueva factura
+	}
+}
--- src/Pago.java
+++ src/Pago.java
@@ -4,10 +4,4 @@ public class Pago {
 	public void realizarCobro(double monto) {
 		//cargar el monto de compra al medio de pago
 	}
-	public void calcularImpuestosFactura() {
-		//calcula los impuestos asociados a la compra
-	}
-	public void generarFactura() {
-		//generar una nueva factura
-	}
 }
